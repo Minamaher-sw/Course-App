fdc925fdca7db0751912865d0d993eba73581847
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-07 16:47:29 +0300

    Initialize Express server with JSON parser and course routes

---
+++ index.js
@@ -0,0 +1,16 @@
+
+import express  from "express";
+import courseRoute from "./routes/course.routes.js";
+const app =express();
+
+// app.use(bodyParser.json());
+// parser
+app.use(express.json());
+
+app.use("/api/courses",courseRoute)
+
+app.listen(3001,"127.0.0.1",511,()=>{
+    console.log("server is connected")
+}).on("error",(err)=>{
+    console.log(err);
+})
