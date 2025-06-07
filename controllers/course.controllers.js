b4dcb2aeb26cc87b5448a8ba23d27f336356e008
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-07 16:49:11 +0300

    Add CRUD operations for courses with basic validation using express-validator

---
+++ controllers/course.controllers.js
@@ -0,0 +1,56 @@
+
+import { validationResult } from "express-validator"
+
+import { courses } from "../data/cousres.data.js"
+const getAllcourses =(req,res)=>{
+    res.json(courses)
+}
+
+const getCourseById =(req,res)=>{
+    const courseId = +req.params.courseId ;
+    const course = courses.find((course)=> course.id === courseId);
+    if(!course){
+        return res.statu(500).json({message:errors.array()})
+    }
+    res.json(course)
+} 
+const createCourse =(req ,res)=>{
+    const errors =validationResult(req.body);
+    if(!errors.isEmpty()){
+        return res.statu(500).json({message:errors.array()})
+    }
+    courses.push({id:courses.length +1 ,...req.body});
+    res.json(courses[courses.length-1])
+}
+
+const updateCourse =(req,res)=>{
+    const courseId = +req.params.courseId ;
+    const inputData =req.body;
+    const course = courses.find((course)=> course.id === courseId);
+    if(!course){
+        return res.statu(500).json({message:errors.array()})
+    }
+    if(inputData.title !== undefined) course.title =inputData.title;
+    if (inputData.description !== undefined) course.description = inputData.description;
+    if (inputData.price !== undefined) course.price = inputData.price;
+    if (inputData.instructor !== undefined) course.instructor = inputData.instructor;
+    if (inputData.duration !== undefined) course.duration = inputData.duration;
+    if (inputData.level !== undefined) course.level = inputData.level;
+    res.json(course);
+}
+const deleteCourse = (req,res)=>{
+    const courseId = +req.params.courseId ;
+    const course = courses.find((course)=> course.id === courseId);
+    if(!course){
+        return res.statu(500).json({message:errors.array()})
+    }
+    res.json(courses.filter((course)=>{ return course.id != courseId}));
+}
+
+export default{
+    getAllcourses,
+    updateCourse,
+    getCourseById,
+    deleteCourse,
+    createCourse
+}
