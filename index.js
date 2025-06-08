53ad44c7ff47330ef63dd44e9270ad826e3b8410
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-08 17:04:11 +0300

    feat: add CORS support and improve error handling in the application

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 
 import express  from "express";
+import cors from "cors";
+
 import courseRoute from "./routes/course.routes.js";
+import httpStatusMessage from "./utils/httpStatusJsend.js"
 const app =express();
 // app.use(bodyParser.json());
+app.use(cors());
 // parser
 app.use(express.json());
 
 app.use("/api/courses",courseRoute)
 
+
+
+// wildcard 
+app.all("*",(req,res,next)=>{
+    res.status(404).json({
+    status :httpStatusMessage.FAIL ,
+    data : {message: "Route not found" }}
+)});
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({
+    status : err.statusText || httpStatusMessage.ERROR ,
+    data : {message: err.message || "Internal Server Error"}}
+)
+})
 
 app.listen(3001,"127.0.0.1",511,()=>{
     console.log("server is connected")
