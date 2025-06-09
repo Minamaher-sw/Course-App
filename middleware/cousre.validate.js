4f236f2310916ac95c97f58564e34e67687a30bc
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-09 20:24:23 +0300

    refactor: remove commented-out validation for single course fields in validation middleware

--- middleware/cousre.validate.js
+++ middleware/cousre.validate.js
@@ -5,38 +5,6 @@ import { body } from "express-validator";
  */
 export const validate = () => {
 return [
-        // Validate single course object fields
-        // body("title")
-        //         .notEmpty()
-        //         .withMessage("title must be not empty")
-        //         .isLength({ min: 3 })
-        //         .withMessage("title length must be more than 3 letters"),
-
-        // body("price")
-        //         .notEmpty()
-        //         .withMessage("price must be not empty")
-        //         .isNumeric()
-        //         .withMessage("price must be a number"),
-
-        // body("description")
-        //         .notEmpty()
-        //         .withMessage("description must be not empty")
-        //         .isLength({ min: 5 })
-        //         .withMessage("description must be at least 5 characters long"),
-
-        // body("instructor")
-        //         .notEmpty()
-        //         .withMessage("instructor must be not empty"),
-
-        // body("duration")
-        //         .notEmpty()
-        //         .withMessage("duration must be not empty"),
-
-        // body("level")
-        //         .notEmpty()
-        //         .withMessage("level must be not empty"),
-
-        // Validate array of course objects: courses[].field
         body("courses").optional().isArray().withMessage("courses must be an array"),
         body("courses.*.title")
                 .notEmpty()
